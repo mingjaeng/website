9618306b2a581e3fa3a5471fe23747e5027409dd
mingjaeng <mjkimjeck@naver.com>
2025-06-25 14:06:46 +0900

    Delete asdasd-main directory

--- asdasd-main/capstone/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('capstone_app.urls')),  # capstone_app의 URL을 사용
-]
--- asdasd-main/capstone_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('detail/<str:company_name>/', views.company_detail, name='company_detail'),
-    path('stock-info/', views.stock_info_api, name='stock_info'),
-]
